b22f6bdc0e5040b759b2c9abf313c594046fb516
Jzulu22x <juanzr1015@gmail.com>
2024-07-18 12:19:27 -0500

    Creacion de la clase libro

---
+++ Models/Libro.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gestion_biblioteca.Models;
+public class Libro : Biblioteca
+{
+    public int Id {get; set;}
+
+    public Libro(int id, double precio, string genero, string isbn, string autor, string titulo)
+    {
+        Id = id;
+        Precio = precio;
+        Genero = genero;
+        ISBN = isbn;
+        Autor = autor;
+        Titulo = titulo;
+    }
+    public void Descripcion(){
+        Console.WriteLine(@$"
+        Id: {Id}
+        Titulo: {Titulo}
+        Año de publicacion: {AñoPublicacion}
+        Autor: {Autor}
+        ISBN: {ISBN}
+        Genero: {Genero}
+        Precio: {Precio}
+        ");
+    }
+    public double Descuento (double porcentaDescuento) {
+        double precioDescuento = Precio - (Precio * porcentaDescuento);
+        return precioDescuento;
+    }
+
+    public List<Libro> OrdenarPorAño(List<Libro> listaAordenar){
+       List<Libro> listaOrdenada = listaAordenar.OrderBy(libro => libro.AñoPublicacion).ToList();
+       return listaOrdenada;
+    }
+    public bool LibroReciente(List<Libro> libros, int añoActual){
+        for (int i = 0; i < libros.Count(); i++)
+        {
+            if(añoActual - libros[i].AñoPublicacion <= 5 ){
+                return true;
+            }
+        }
+        return false;
+    }
+}
