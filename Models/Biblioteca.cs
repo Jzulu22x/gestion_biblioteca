232f31e7672b6c2dcb0bd4496b23d151c0983d5b
Jzulu22x <juanzr1015@gmail.com>
2024-07-18 12:04:09 -0500

    Cambio de tipo de dato a double de la variable Precio

--- Models/Biblioteca.cs
+++ Models/Biblioteca.cs
@@ -7,49 +7,63 @@ using gestion_biblioteca.Models;
 namespace gestion_biblioteca.Models;
 public class Biblioteca : Publicacion
 {
-    public string? Autor {get; set;}
-    public string? ISBN {get; set;}
-    public string? Genero {get; set;}
-    public string? Precio {get; set;}
+    public string? Autor { get; set; }
+    public string? ISBN { get; set; }
+    public string? Genero { get; set; }
+    public double Precio { get; set; }
 
-    public void EliminarLibro(List<Libro> listaLibros, string titulo){
-        for (int i = 0; i < listaLibros.Count(); i++){
-            if(listaLibros[i].Titulo == titulo){
+    public void EliminarLibro(List<Libro> listaLibros, string titulo)
+    {
+        for (int i = 0; i < listaLibros.Count(); i++)
+        {
+            if (listaLibros[i].Titulo == titulo)
+            {
                 listaLibros.RemoveAt(i);
             }
-            else {
+            else
+            {
                 Console.WriteLine("Error, libro no encontrado");
             }
         }
     }
-    public void AgregarLibro (List<Libro> listaLibros, Libro libro) {
+    public void AgregarLibro(List<Libro> listaLibros, Libro libro)
+    {
         listaLibros.Add(libro);
     }
-    public List<Libro> BuscarLibroAutor(List<Libro> listaLibros, string autor){
-            List<Libro> librosEncontrados = new List<Libro>();
-            for(int i = 0; i< listaLibros.Count(); i++){
-                if(listaLibros[i].Autor == autor ) {
-                    librosEncontrados.Add(listaLibros[i]);
-                }
+    public List<Libro> BuscarLibroAutor(List<Libro> listaLibros, string autor)
+    {
+        List<Libro> librosEncontrados = new List<Libro>();
+        for (int i = 0; i < listaLibros.Count(); i++)
+        {
+            if (listaLibros[i].Autor == autor)
+            {
+                librosEncontrados.Add(listaLibros[i]);
             }
-            return librosEncontrados;
+        }
+        return librosEncontrados;
     }
-    public List<Libro> BuscarLibroGenero(List<Libro> listaLibros, string genero){
-            List<Libro> librosEncontrados = new List<Libro>();
-            for(int i = 0; i< listaLibros.Count(); i++){
-                if(listaLibros[i].Genero == genero ) {
-                    librosEncontrados.Add(listaLibros[i]);
-                }
+    public List<Libro> BuscarLibroGenero(List<Libro> listaLibros, string genero)
+    {
+        List<Libro> librosEncontrados = new List<Libro>();
+        for (int i = 0; i < listaLibros.Count(); i++)
+        {
+            if (listaLibros[i].Genero == genero)
+            {
+                librosEncontrados.Add(listaLibros[i]);
             }
-            return librosEncontrados;
+        }
+        return librosEncontrados;
     }
-    public List<Libro> BuscarLibroRango(List<Libro> listaLibros, int añoInicial, int añofinal){
-            List<Libro> librosEncontrados = new List<Libro>();
-            for(int i = 0; i< listaLibros.Count(); i++){
-                if(listaLibros[i].AñoPublicacion >= añoInicial && listaLibros[i].AñoPublicacion <= añofinal ) {
-                    librosEncontrados.Add(listaLibros[i]);
-                }
+    public List<Libro> BuscarLibroRango(List<Libro> listaLibros, int añoInicial, int añofinal)
+    {
+        List<Libro> librosEncontrados = new List<Libro>();
+        for (int i = 0; i < listaLibros.Count(); i++)
+        {
+            if (listaLibros[i].AñoPublicacion >= añoInicial && listaLibros[i].AñoPublicacion <= añofinal)
+            {
+                librosEncontrados.Add(listaLibros[i]);
             }
-            return librosEncontrados;
+        }
+        return librosEncontrados;
     }
 }
